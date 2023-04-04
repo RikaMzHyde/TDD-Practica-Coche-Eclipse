cb92ba7cb445e8b360cd703e37a49becaba5a7a9
Laura Pinedo <laurampp95@gmail.com>
2023-04-04 13:49:23 +0200

    Creada rama "memoria" para subir la memoria del ejercicio

--- src/Coche.java
+++ src/Coche.java
@@ -5,13 +5,10 @@ public class Coche {
 
 	public void acelerar_Laura_Pinedo(int aceleracion) {
 		velocidad += aceleracion;
-		
 	}
-
 	public void decelerar_Laura_Pinedo(int deceleracion) {
 		velocidad -= deceleracion;
 		if(velocidad < 0) velocidad = 0;
-		
 	}
 
 }
