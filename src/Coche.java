dbd809eb415810c082ae559d68ceed0f93adf5ae
Laura Pinedo <laurampp95@gmail.com>
2023-04-04 13:47:40 +0200

    Refactorizamos los nombres de los métodos añadiendo mi nombre y apellido

--- src/Coche.java
+++ src/Coche.java
@@ -3,12 +3,12 @@ public class Coche {
 
 	public Integer velocidad = 0;
 
-	public void acelerar(int aceleracion) {
+	public void acelerar_Laura_Pinedo(int aceleracion) {
 		velocidad += aceleracion;
 		
 	}
 
-	public void decelerar(int deceleracion) {
+	public void decelerar_Laura_Pinedo(int deceleracion) {
 		velocidad -= deceleracion;
 		if(velocidad < 0) velocidad = 0;
 		
